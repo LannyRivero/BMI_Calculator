12e2b522930d4a44e2b5a72c5315e1022562b3d9
lannyRivero <lannyrivero288@gmail.com>
2025-03-24 20:32:58 +0100

    test: add failing test for Person getters(red flag)

--- src/test/java/dev/lanny/bmi_calculator/model/PersonTest.java
+++ src/test/java/dev/lanny/bmi_calculator/model/PersonTest.java
@@ -1,5 +1,7 @@
 package dev.lanny.bmi_calculator.model;
 
+import static org.hamcrest.MatcherAssert.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +10,15 @@ public class PersonTest {
     @Test
     @DisplayName("Validate  Person Creation")
     void test_Person_Creation() {
-        new Person(70.0, 1.75); 
+        new Person(70.0, 1.75);
+    }
+
+    @Test
+    @DisplayName("Validate Person Getters")
+    void test_Person_Getters() {
+        Person person = new Person(70.0, 1.75);
+
+        assertThat(person.getWeight(), is(70.0));
+        assertThat(person.getHeight(), is(1.75));
     }
 }
