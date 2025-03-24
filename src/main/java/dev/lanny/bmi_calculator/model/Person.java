1a8ecd7b198fa8a5838457e93d186e9520c2969c
lannyRivero <lannyrivero288@gmail.com>
2025-03-24 20:34:49 +0100

    feat: implement getters for Person attributes(green flag)

--- src/main/java/dev/lanny/bmi_calculator/model/Person.java
+++ src/main/java/dev/lanny/bmi_calculator/model/Person.java
@@ -8,4 +8,7 @@ public class Person {
         this.weight = weight;
         this.height = height;
     }
+
+    public double getWeight() { return weight; }
+    public double getHeight() { return height; }
 }
