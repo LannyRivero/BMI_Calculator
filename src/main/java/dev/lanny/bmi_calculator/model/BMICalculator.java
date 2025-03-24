3209cbf1cbdfa49ec0280eb65851fbe2370589fa
lannyRivero <lannyrivero288@gmail.com>
2025-03-24 22:32:52 +0100

    feat: validate inputs in calculateBMI method(green flag)

--- src/main/java/dev/lanny/bmi_calculator/model/BMICalculator.java
+++ src/main/java/dev/lanny/bmi_calculator/model/BMICalculator.java
@@ -1,8 +1,11 @@
 package dev.lanny.bmi_calculator.model;
 
 public class BMICalculator {
-    
+
     public double calculateBMI(double weight, double height) {
+        if (weight <= 0 || height <= 0) {
+            throw new IllegalArgumentException("El peso y la altura deben ser mayores que cero.");
+        }
         return weight / Math.pow(height, 2);
     }
 }
