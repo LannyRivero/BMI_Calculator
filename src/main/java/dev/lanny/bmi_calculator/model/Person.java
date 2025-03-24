9817541fa807e16df1785a92ff0fb72ff0af0ac8
lannyRivero <lannyrivero288@gmail.com>
2025-03-24 20:37:05 +0100

    feat: add validation to Person constructor(green flag)

--- src/main/java/dev/lanny/bmi_calculator/model/Person.java
+++ src/main/java/dev/lanny/bmi_calculator/model/Person.java
@@ -5,6 +5,9 @@ public class Person {
     private double height;
 
     public Person(double weight, double height) {
+        if (weight <= 0 || height <= 0) {
+            throw new IllegalArgumentException("El peso y la altura deben ser mayores que cero.");
+        }
         this.weight = weight;
         this.height = height;
     }
