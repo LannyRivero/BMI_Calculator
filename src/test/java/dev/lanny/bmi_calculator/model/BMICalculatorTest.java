16804c8b269d4f5423e8e15f77b0f3ddfaec7036
lannyRivero <lannyrivero288@gmail.com>
2025-03-24 22:32:12 +0100

    test: add failing test for calculateBMI with invalid inputs(red flag)

--- src/test/java/dev/lanny/bmi_calculator/model/BMICalculatorTest.java
+++ src/test/java/dev/lanny/bmi_calculator/model/BMICalculatorTest.java
@@ -2,6 +2,8 @@ package dev.lanny.bmi_calculator.model;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.closeTo;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +14,18 @@ public class BMICalculatorTest {
     void test_Calculate_BMI_Valid_Values() {
         BMICalculator calculator = new BMICalculator();
         double bmi = calculator.calculateBMI(70.0, 1.75);
-        assertThat(bmi, closeTo(22.86, 0.01)); 
+        assertThat(bmi, closeTo(22.86, 0.01));
+    }
+
+    @Test
+    @DisplayName("Validate throw exception when weight or height is zero or negative")
+    void test_Calculate_BMI_Invalid_Values() {
+        BMICalculator calculator = new BMICalculator();
+
+        assertThrows(IllegalArgumentException.class, () -> calculator.calculateBMI(0, 1.75));
+        assertThrows(IllegalArgumentException.class, () -> calculator.calculateBMI(70, 0));
+        assertThrows(IllegalArgumentException.class, () -> calculator.calculateBMI(-70, 1.75));
+        assertThrows(IllegalArgumentException.class, () -> calculator.calculateBMI(70, -1.75));
     }
 
 }
