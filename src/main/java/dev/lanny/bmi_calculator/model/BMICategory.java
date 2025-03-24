7195b061092308b85596e7cc2999829ffcd4c85b
lannyRivero <lannyrivero288@gmail.com>
2025-03-24 22:01:01 +0100

    feat: implement OBESITY II BMI category to pass test(green flag)

--- src/main/java/dev/lanny/bmi_calculator/model/BMICategory.java
+++ src/main/java/dev/lanny/bmi_calculator/model/BMICategory.java
@@ -3,7 +3,8 @@ package dev.lanny.bmi_calculator.model;
 public enum BMICategory {
     NORMAL("Peso Normal", 18.5, 25),
     OVERWEIGHT("Sobrepeso", 25, 30),
-    OBESITY_I("Obesidad tipo I", 30, 35);
+    OBESITY_I("Obesidad tipo I", 30, 35),
+    OBESITY_II("Obesidad tipo II", 35, 40);
     
 
     private final String description;
@@ -26,6 +27,10 @@ public enum BMICategory {
         if (bmi >= 30 && bmi < 35) {
             return "Obesidad tipo I";
         }
+        if (bmi >= 35 && bmi < 40) {
+            return "Obesidad tipo II";
+        }
+        
         return "Valor inválido";
     }
     
