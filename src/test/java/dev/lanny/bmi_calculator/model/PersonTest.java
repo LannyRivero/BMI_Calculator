ccb403e938fff7aeceae7d3cc0f0443e6110baeb
lannyRivero <lannyrivero288@gmail.com>
2025-03-24 20:36:10 +0100

    test: add failing test for Person constructor validation (red flag)

--- src/test/java/dev/lanny/bmi_calculator/model/PersonTest.java
+++ src/test/java/dev/lanny/bmi_calculator/model/PersonTest.java
@@ -1,6 +1,8 @@
 package dev.lanny.bmi_calculator.model;
 
 import static org.hamcrest.MatcherAssert.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,5 +22,11 @@ public class PersonTest {
 
         assertThat(person.getWeight(), is(70.0));
         assertThat(person.getHeight(), is(1.75));
+    }
+
+    @Test
+    void test_Person_Invalid_Weight_And_Height() {
+        assertThrows(IllegalArgumentException.class, () -> new Person(0, 1.75));
+        assertThrows(IllegalArgumentException.class, () -> new Person(70.0, 0));
     }
 }
